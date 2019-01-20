c60569386a852a95cef98a455fbffb5de1ec3fe2
blastbeatsandcode <alexsilcott@gmail.com>
2019-01-20 10:58:25 -0500

    Initial enum and component class code implemented.

--- TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/model/components/Component.java
+++ TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/model/components/Component.java
@@ -3,9 +3,7 @@ package com.blastbeatsandcode.trashcan.model.components;
 import com.blastbeatsandcode.trashcan.model.states.StatusEnum;
 
 public interface Component {
-    private StatusEnum _status;
-
-    public StatusEnum getStatus();
-    public boolean turnOn();
-    public boolean turnOff();
+    StatusEnum getStatus();
+    boolean turnOn();
+    boolean turnOff();
 }
