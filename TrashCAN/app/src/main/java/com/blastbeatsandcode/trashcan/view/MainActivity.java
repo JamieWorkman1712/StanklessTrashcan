3694785aecafe2a1a4f9acf1f807aaae0634013b
blastbeatsandcode <alexsilcott@gmail.com>
2019-01-20 11:50:58 -0500

    Added some temporary text to show when the app starts up.

--- TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/view/MainActivity.java
+++ TrashCAN/app/src/main/java/com/blastbeatsandcode/trashcan/view/MainActivity.java
@@ -1,8 +1,11 @@
 package com.blastbeatsandcode.trashcan.view;
 
+import android.graphics.Color;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
 
 import com.blastbeatsandcode.trashcan.R;
 import com.blastbeatsandcode.trashcan.controller.CanController;
@@ -15,6 +18,12 @@ public class MainActivity extends AppCompatActivity implements TrashCanView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tempLabel = (TextView) findViewById(R.id.lblTempLabel);
+        tempLabel.setTextSize(32f);
+        tempLabel.setTextColor(Color.BLACK);
+        tempLabel.setText("TrashCAN with Stankless Technology\n\nBrought to you by the guys who got stuck in a group together but it actually worked out pretty okay.\n\n-Trashbois");
+        tempLabel.setGravity(Gravity.CENTER_HORIZONTAL);
 
         Messages.makeToast(this, "Application loaded! Time to get STANKLESS!");
     }
